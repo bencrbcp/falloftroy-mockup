ef5dea33e1ff9d1745a542251fa17aa1c0203e4f
bencrbcp <bencrbcp@gmail.com>
2020-06-25 20:47:10 -0400

    volume 20

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ else {
 }
 
 var playState = false;
-var music = new Sound('music/fcpremix.mp3', 0, true)
+var music = new Sound('music/fcpremix.mp3', 20, true)
 
 function ready() {
     var playButton = document.getElementsByClassName('btn-play')[0]
